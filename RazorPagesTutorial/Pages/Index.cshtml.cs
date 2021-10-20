4306ed59d277288094bf4a61eb52776379521c15
YL <simon-ling@hotmail.com>
2021-10-20 19:08:51 -0400

    done

---
+++ RazorPagesTutorial/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesTutorial.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        //http get request
+        public void OnGet()
+        {
+            Message = "Hello World! It is "+DateTime.Now.ToLongTimeString()+" now.";
+            //Now, Message is available at Index.cshtml
+        }
+    }
+}
