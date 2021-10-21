63671e4d7efc0ee883fd0cfff434fc5f5ef4965b
YL <simon-ling@hotmail.com>
2021-10-21 19:10:57 -0400

    done

---
+++ RazorPagesTutorial/EvenConstraint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace RazorPagesTutorial
+{
+    public class EvenConstraint : IRouteConstraint
+    {
+        public bool Match(
+            HttpContext httpContext, 
+            IRouter route, string routeKey, 
+            RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            int id;
+            if (Int32.TryParse(values["id"].ToString(), out id))
+            {
+                if (id%2==0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- RazorPagesTutorial/Startup.cs
+++ RazorPagesTutorial/Startup.cs
@@ -34,6 +34,7 @@ namespace RazorPagesTutorial
                 options.LowercaseUrls = true;
                 options.LowercaseQueryStrings = true;
                 options.AppendTrailingSlash = true;
+                options.ConstraintMap.Add("even",typeof(EvenConstraint));
             });
         }
 
