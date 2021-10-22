5debc2dd25fbcfd91fb9ac429c56252583e0b1a2
YL <simon-ling@hotmail.com>
2021-10-21 21:08:01 -0400

    done

---
+++ RazorPagesTutorial/Pages/Employees/Edit.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Models;
+using RazorPagesTutorial.Services;
+
+namespace RazorPagesTutorial.Pages.Employees
+{
+    public class EditModel : PageModel
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EditModel(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public Employee Employee { get; private set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Employee= _employeeRepository.GetEmployee(id);
+            if (Employee==null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
