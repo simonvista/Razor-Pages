dc83e292f8620bca393076b8d0a6d8d42909d4d0
YL <simon-ling@hotmail.com>
2021-10-24 20:46:51 -0400

    done

---
+++ RazorPagesTutorial/Pages/V2/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesTutorial.Models;
+using RazorPagesTutorial.Services;
+
+namespace RazorPagesTutorial.Pages.V2
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesTutorial.Services.AppDbContext _context;
+
+        public CreateModel(RazorPagesTutorial.Services.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Employees.Add(Employee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
